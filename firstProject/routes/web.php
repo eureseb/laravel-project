369f9d13ec176243ff558f1ba56bc39c9aa5e0c6
eureseb <euresebb@gmail.com>
2021-12-08 01:55:56 +0800

    nothing much

--- firstProject/routes/web.php
+++ firstProject/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\http\Controllers\PagesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//Laravel 8(New)
+Route::get('/', [PagesController::class, 'index']);
